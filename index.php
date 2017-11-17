469a059e733695ff9ad5c82dfa5a84120bd3cf6c
Your Name <you@example.com>
2017-11-17 11:09:57 +0100

    More input

--- index.php
+++ index.php
@@ -15,7 +15,12 @@
                         <li><a href="index.php">Home</a></li>
                     </ul>    
                     <div class="nav-login">
-                        
+                        <form>
+                            <input type="text" name="uid" placeholder="Username/e-mail">
+                            <input type="text" name="pwd" placeholder="password" >
+                            <button type="submit" name="submit">Login</button>
+                        </form>
+                        <a href="signup.php">Sign up</a>
                     </div>
                 </div>
             </nav>
