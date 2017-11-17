d475f0996db38957a8d7a6e46ad69f007ddb7928
Your Name <you@example.com>
2017-11-17 10:54:01 +0100

    Home Button

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>hi!</title>
+        
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="main-wrapper">
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                    </ul>    
+                    <div class="nav-login">
+                        
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <section>
+            
+            
+        </section>
+    </body>
+</html>
